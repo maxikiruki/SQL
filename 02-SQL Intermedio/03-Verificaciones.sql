14cf38adf92068e7d9e183f27e220d7cdab83132
Jaime Navarro <57503118+maxikiruki@users.noreply.github.com>
2025-11-05 21:06:48 +0100

    Añadido 03, Diagrama

---
+++ 02-SQL Intermedio/03-Verificaciones.sql
@@ -0,0 +1,30 @@
+-- *** VERIFICACIONES *** --
+
+
+
+-- Conectarnos a la base de datos:
+USE spotifysimulator
+
+-- Verificar tabla Géneros:
+SELECT *
+    FROM Generos;
+
+-- Verificar tabla Artistas:
+SELECT *
+    FROM Artistas;
+
+-- Verificar tabla Canciones:
+SELECT *
+    FROM Canciones;
+
+-- Verificar tabla Usuarios:
+SELECT *
+    FROM Usuarios;
+
+-- Verificar tabla Amigos:
+SELECT *
+    FROM Amigos;
+
+-- Verificar tabla Reproducciones:
+SELECT *
+    FROM Reproducciones;
